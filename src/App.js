45f9370f58611ffb803f683da32804e42078f316
nghianv-1868 <ngo.van.nghia@sun-asterisk.com>
2021-02-02 18:07:17 +0700

    page-detail-nft

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import NavBar from 'Components/NavBar';
 
 import Home from 'Views/Home';
 import Profile from 'Views/Profile';
+import DetailNFT from 'Views/DetailNFT';
 
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <div className='center'>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/profile' component={Profile} />
-          <Route exact path='/profile/:id' component={Profile} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className='root PE'>
+      <BrowserRouter>
+        <NavBar />
+        <div className='page center content'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/profile' component={Profile} />
+            <Route exact path='/profile/:id' component={Profile} />
+            <Route exact path='/token/:addressToken/:id' component={DetailNFT} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
