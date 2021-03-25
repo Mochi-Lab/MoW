405fd1b942e3ef120de5a43e61b4af620f1c2462
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2021-03-25 11:05:34 +0700

    feat : browse for search erc721

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import store from 'store/index';
 import { useEffect } from 'react';
 import CreateERC721 from 'Views/Create/ERC721';
 import CreateERC1155 from 'Views/Create/ERC1155';
+import Browse from 'Views/Browse';
 
 function App() {
   useEffect(() => {
@@ -36,6 +37,7 @@ function App() {
             <Route exact path='/profile' component={Profile} />
             <Route exact path='/submit-Nfts' component={SubmitNFT} />
             <Route exact path='/create' component={Create} />
+            <Route exact path='/browse' component={Browse} />
             <Route exact path='/create/erc721' component={CreateERC721} />
             <Route exact path='/create/erc1155' component={CreateERC1155} />
             <Route exact path='/token/:addressToken/:id' component={DetailNFT} />
